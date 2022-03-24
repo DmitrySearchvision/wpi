27d7c80de8015ee4f0a8fb6a1c2375336f257d71
Dmitry Egorov <d.egorov.1986@gmail.com>
2022-03-24 10:52:04 +0200

    fix--wp_check_invalid_utf8

--- fix_normalize_whitespace.sh
+++ fix_normalize_whitespace.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-echo '----- START fixing normalize_whitespace() ------'
+echo '----- START fixing php8 ------'
 
 curl -o formatting.wzc.php https://raw.githubusercontent.com/DmitrySearchvision/wpi/main/formatting.wzc.php
 
 if test -f "formatting.wzc.php"; then
   mv formatting.wzc.php web/wp/wp-includes/formatting.wzc.php
   sed -i 's/function normalize_whitespace( $str ) {/function normalize_whitespace_old( $str ) {/' web/wp/wp-includes/formatting.php
+  sed -i 's/function wp_check_invalid_utf8( $string, $strip = false ) {/function wp_check_invalid_utf8_old( $string, $strip = false ) {/' web/wp/wp-includes/formatting.php
   sed -i "s/require_once 'formatting.wzc.php';//" web/wp/wp-includes/formatting.php
   echo "require_once 'formatting.wzc.php';" >> web/wp/wp-includes/formatting.php
   echo 'OK normalize_whitespace() is fixed'
@@ -14,4 +15,4 @@ else
   echo "ERROR: formatting.wzc.php doesn't exist"
 fi
 
-echo '----- END fixing normalize_whitespace() ------'
+echo '----- END fixing php8 ------'
--- formatting.wzc.php
+++ formatting.wzc.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * Normalize EOL characters and strip duplicate whitespace.
+ *
+ * @since 2.7.0
+ *
+ * @param string $str The string to normalize.
+ * @return string The normalized string.
+ */
 function normalize_whitespace( $str ) {
 	if ( ! is_array( $str ) ) {
 		$str = trim( $str );
@@ -8,4 +16,57 @@ function normalize_whitespace( $str ) {
 	}
 
 	return $str;
+}
+
+/**
+ * Checks for invalid UTF8 in a string.
+ *
+ * @since 2.8.0
+ *
+ * @param string $string The text which is to be checked.
+ * @param bool   $strip  Optional. Whether to attempt to strip out invalid UTF8. Default false.
+ * @return string The checked text.
+ */
+function wp_check_invalid_utf8( $string, $strip = false ) {
+    if (is_array($string)) {
+        $string = $string[0] ?? '';
+    }
+
+    $string = (string) $string;
+
+    if ( 0 === strlen( $string ) ) {
+        return '';
+    }
+
+    // Store the site charset as a static to avoid multiple calls to get_option().
+    static $is_utf8 = null;
+    if ( ! isset( $is_utf8 ) ) {
+        $is_utf8 = in_array( get_option( 'blog_charset' ), array( 'utf8', 'utf-8', 'UTF8', 'UTF-8' ), true );
+    }
+    if ( ! $is_utf8 ) {
+        return $string;
+    }
+
+    // Check for support for utf8 in the installed PCRE library once and store the result in a static.
+    static $utf8_pcre = null;
+    if ( ! isset( $utf8_pcre ) ) {
+        // phpcs:ignore WordPress.PHP.NoSilencedErrors.Discouraged
+        $utf8_pcre = @preg_match( '/^./u', 'a' );
+    }
+    // We can't demand utf8 in the PCRE installation, so just return the string in those cases.
+    if ( ! $utf8_pcre ) {
+        return $string;
+    }
+
+    // phpcs:ignore WordPress.PHP.NoSilencedErrors.Discouraged -- preg_match fails when it encounters invalid UTF8 in $string.
+    if ( 1 === @preg_match( '/^./us', $string ) ) {
+        return $string;
+    }
+
+    // Attempt to strip the bad chars if requested (not recommended).
+    if ( $strip && function_exists( 'iconv' ) ) {
+        return iconv( 'utf-8', 'utf-8', $string );
+    }
+
+    return '';
 }
